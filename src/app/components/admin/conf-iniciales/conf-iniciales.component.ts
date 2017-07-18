32fe3085094a4cc47a4926232f4da4a673b32c91
cococoquito <adiz98g@gmail.com>
2017-07-17 21:43:29 -0500

    commit

--- src/app/components/admin/conf-iniciales/conf-iniciales.component.ts
+++ src/app/components/admin/conf-iniciales/conf-iniciales.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Componente para las configuraciones iniciales del sistema
+ */
 @Component({
   selector: 'app-conf-iniciales',
   templateUrl: './conf-iniciales.component.html',
@@ -7,9 +10,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfInicialesComponent implements OnInit {
 
+  /** banderas para la visualizacion de cada configuracion */
+  private tiposViviendasB: boolean;
+  private tiposVehiculosB: boolean;
+  private tiposCuentasB: boolean;
+  private mensualidadViviendasB: boolean;
+  private mensualidadVehiculosB: boolean;
+
+  /** style para el tab seleccionado */
+  private style_tab_selected = {
+    'background-color': '#EEEEEE'
+  };
+
+  /** style para el tab NO seleccionado */
+  private style_tab_not_selected = {
+    'cursor': 'pointer'
+  };
+
   constructor() { }
 
-  ngOnInit() {
+  /**
+   * Metodo que permite inicializar las variables del component
+   */
+  ngOnInit(): void {
   }
 
+  /**
+   * Metodo que permite visualizar la parametrizacion para tipos de viviendas
+   */
+  private showTiposVivienda(): void {
+    this.tiposViviendasB = true;
+    this.tiposVehiculosB = false;
+    this.tiposCuentasB = false;
+    this.mensualidadViviendasB = false;
+    this.mensualidadVehiculosB = false;
+  }
+
+  /**
+   * Metodo que permite visualizar la parametrizacion para tipos de vehiculos
+   */
+  private showTiposVehiculos(): void {
+    this.tiposViviendasB = false;
+    this.tiposVehiculosB = true;
+    this.tiposCuentasB = false;
+    this.mensualidadViviendasB = false;
+    this.mensualidadVehiculosB = false;
+
+  }
+
+  /**
+   * Metodo que permite visualizar la parametrizacion para tipos de cuentas
+   */
+  private showTiposCuentas(): void {
+    this.tiposViviendasB = false;
+    this.tiposVehiculosB = false;
+    this.tiposCuentasB = true;
+    this.mensualidadViviendasB = false;
+    this.mensualidadVehiculosB = false;
+  }
+
+  /**
+   * Metodo que permite visualizar la parametrizacion para mensualidad de vivienda
+   */
+  private showMensualidadVivienda(): void {
+    this.tiposViviendasB = false;
+    this.tiposVehiculosB = false;
+    this.tiposCuentasB = false;
+    this.mensualidadViviendasB = true;
+    this.mensualidadVehiculosB = false;
+  }
+
+  /**
+   * Metodo que permite visualizar la parametrizacion para mensualidad de vehiculos
+   */
+  private showMensualidadVehiculo(): void {
+    this.tiposViviendasB = false;
+    this.tiposVehiculosB = false;
+    this.tiposCuentasB = false;
+    this.mensualidadViviendasB = false;
+    this.mensualidadVehiculosB = true;
+  }
 }
