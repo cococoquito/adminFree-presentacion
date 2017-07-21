22dc30e6f97b5384c762da1a108d36743ce91bad
cococoquito <adiz98g@gmail.com>
2017-07-20 21:49:34 -0500

    commit

--- src/app/components/admin/conf-iniciales/conf-iniciales.component.ts
+++ src/app/components/admin/conf-iniciales/conf-iniciales.component.ts
@@ -1,4 +1,4 @@
-import { TIPOS_VIVIENDAS, ESTADO_ACTIVO } from './../../../util/Constants';
+import { ESTADO_ACTIVO } from './../../../util/Constants';
 import { CommonDTO } from './../../../model/CommonDTO';
 import { AlertService } from './../../../service/alert.service';
 import { UtilitarioService } from './../../../service/utilitario.service';
@@ -16,12 +16,12 @@ import { ConfirmDialogModule, ConfirmationService } from 'primeng/primeng';
 })
 export class ConfInicialesComponent implements OnInit {
 
-  /** banderas para la visualizacion de cada configuracion */
-  private tiposViviendasB: boolean;
-  private tiposVehiculosB: boolean;
-  private tiposCuentasB: boolean;
-  private mensualidadViviendasB: boolean;
-  private mensualidadVehiculosB: boolean;
+  /** Constantes para identificar el tipo de configuracion a registrar */
+  private TIPOS_VIVIENDAS: number = 1;
+  private TIPOS_VEHICULOS: number = 2;
+  private TIPOS_CUENTAS: number = 3;
+  private MENSUALIDAD_VIVIENDA: number = 4;
+  private MENSUALIDAD_VEHICULOS: number = 5;
 
   /** style para el tab seleccionado */
   private style_tab_selected = {
@@ -33,14 +33,14 @@ export class ConfInicialesComponent implements OnInit {
     'cursor': 'pointer'
   };
 
-  /** lista de tipos de configuraciones a visualizar en pantalla */
+  /** lista de los items a visualizar por pantalla */
   private items: Array<CommonDTO>;
 
   /** es el item agregar o editar */
   private itemAgregarEditar: CommonDTO;
 
-  /** inidica que tipo de registro el usuario va parametrizar */
-  private tipoRegistro: number;
+  /** indica el tipo de configuracion que el user va a registrar */
+  private tipoConfiguracion: number;
 
   /** bandera que identifica si ya se hizo submit */
   private submitted: boolean;
@@ -64,50 +64,33 @@ export class ConfInicialesComponent implements OnInit {
   ngOnInit(): void { }
 
   /**
-   * Metodo que permite establecer que el user ya hizo submitted
+   * Metodo que visualiza el tab para las configuracion de los items del sistema
+   * @param tipoConf , indica el tipo de configuracion que el user va registrar
    */
-  private onSubmit(): boolean {
-    this.alertService.hiddenAlert();
-    this.submitted = true;
-    return this.submitted;
-  }
+  private showTabConfiguracionItem(tipoConf: number): void {
 
-  /**
-   * Metodo que permite visualizar la parametrizacion para tipos de viviendas
-   */
-  private showTiposVivienda(): void {
-
-    // solo aplica si no se ha dado click con anterioridad
-    if (!this.tiposViviendasB) {
-
-      // se inicia como si el user no hay dado commit
-      this.submitted = false;
-
-      // inidica que tipo de registro es, se utiliza para guardar, editar, eliminar
-      this.tipoRegistro = TIPOS_VIVIENDAS;
-
-      // es el item para agregar o editar, se utiliza tambien para listar las viviendas
-      this.itemAgregarEditar = new CommonDTO();
-      this.itemAgregarEditar.tipoRegistro = this.tipoRegistro;
-      this.itemAgregarEditar.estado = ESTADO_ACTIVO;
+    // se identifica si este tipo configuracion ya fue seleccionado
+    if (tipoConf != this.tipoConfiguracion) {
 
       // se muestra el modal de carga
       this.utilService.displayLoading(true);
 
-      // se habilita el tab tipos de viviendas
-      this.tiposViviendasB = true;
-      this.tiposVehiculosB = false;
-      this.tiposCuentasB = false;
-      this.mensualidadViviendasB = false;
-      this.mensualidadVehiculosB = false;
+      // se establece el tipo de configuracion
+      this.tipoConfiguracion = tipoConf;
+
+      // se reinicia el submmit
+      this.submitted = false;
+
+      // es el item para agregar o editar, se utiliza tambien para listar los registros
+      this.initItemAgregarEditar();
 
       // son los items a mostrar por pantalla
       this.items = new Array<CommonDTO>();
 
-      // se procede a listar los tipos de vivienda
+      // se procede a listar los registros
       this.adminFreeService.listarItems(this.itemAgregarEditar).subscribe(
         data => {
-          // se configuran los tipos de viviendas retornados
+          // se configuran los registros retornados
           this.items = data.json();
 
           // se cierra el modal de carga
@@ -125,75 +108,58 @@ export class ConfInicialesComponent implements OnInit {
   }
 
   /**
-   * Metodo que permite visualizar la parametrizacion para tipos de vehiculos
-   */
-  private showTiposVehiculos(): void {
-    this.tiposViviendasB = false;
-    this.tiposVehiculosB = true;
-    this.tiposCuentasB = false;
-    this.mensualidadViviendasB = false;
-    this.mensualidadVehiculosB = false;
-
-  }
-
-  /**
-   * Metodo que permite visualizar la parametrizacion para tipos de cuentas
-   */
-  private showTiposCuentas(): void {
-    this.tiposViviendasB = false;
-    this.tiposVehiculosB = false;
-    this.tiposCuentasB = true;
-    this.mensualidadViviendasB = false;
-    this.mensualidadVehiculosB = false;
-  }
-
-  /**
    * Metodo que permite visualizar la parametrizacion para mensualidad de vivienda
    */
   private showMensualidadVivienda(): void {
-    this.tiposViviendasB = false;
-    this.tiposVehiculosB = false;
-    this.tiposCuentasB = false;
-    this.mensualidadViviendasB = true;
-    this.mensualidadVehiculosB = false;
+    this.tipoConfiguracion = this.MENSUALIDAD_VIVIENDA;
   }
 
   /**
    * Metodo que permite visualizar la parametrizacion para mensualidad de vehiculos
    */
   private showMensualidadVehiculo(): void {
-    this.tiposViviendasB = false;
-    this.tiposVehiculosB = false;
-    this.tiposCuentasB = false;
-    this.mensualidadViviendasB = false;
-    this.mensualidadVehiculosB = true;
+    this.tipoConfiguracion = this.MENSUALIDAD_VEHICULOS;
   }
 
   /**
-   * Metodo que permite soportar el evento del boton guardar
+   * Metodo que permite establecer que el user ya hizo submitted
+   */
+  private onSubmit(): boolean {
+    this.submitted = true;
+    return this.submitted;
+  }
+
+  /**
+   * Metodo que permite soportar el evento del boton guardar para los items
    */
   private guardarItem(): void {
+
+    // se valida que si exista un item para agregar o editar
     if (this.itemAgregarEditar) {
 
-      this.itemAgregarEditar.tipoRegistro = this.tipoRegistro;
-      // son los items a mostrar por pantalla
+      // se define el tipo de registro (VIVIENDA, VEHICULO, CUENTA etc)
+      this.itemAgregarEditar.tipoRegistro = this.tipoConfiguracion;
+
+      // se reinicia los items a visualizar
       this.items = new Array<CommonDTO>();
 
+      // si el item no tiene ID es por que es nuevo
       if (!this.itemAgregarEditar.id) {
 
-        // se procede a listar los tipos de vivienda
+        // se procede a registrar el item
         this.adminFreeService.agregarItem(this.itemAgregarEditar).subscribe(
           data => {
-            // se configuran los tipos de viviendas retornados
+            // se configuran los items retornados
             this.items = data.json().result;
 
-            this.alertService.showAlert(data.json().mensaje, "alert alert-success text_center", false);
+            // se reinicia el item de agregacion o edicion
+            this.initItemAgregarEditar();
+
+            // se reinicia el submmit
+            this.submitted = false;
 
             // se cierra el modal de carga
             this.utilService.displayLoading(false);
-
-            // se inicia como si el user no hay dado commit
-            this.submitted = false;
           },
           error => {
             // se muestra el mensaje alert danger
@@ -203,21 +169,21 @@ export class ConfInicialesComponent implements OnInit {
             this.utilService.displayLoading(false);
           }
         )
-
       } else {
-        // se procede a listar los tipos de vivienda
+        // si el item tiene id se procede actualizar su nombre
         this.adminFreeService.editarItem(this.itemAgregarEditar).subscribe(
           data => {
-            // se configuran los tipos de viviendas retornados
+            // se configuran los items retornados
             this.items = data.json().result;
 
-            this.alertService.showAlert(data.json().mensaje, "alert alert-success text_center", false);
+            // se reinicia el item de agregacion o edicion
+            this.initItemAgregarEditar();
+
+            // se reinicia el submmit
+            this.submitted = false;
 
             // se cierra el modal de carga
             this.utilService.displayLoading(false);
-
-            // se inicia como si el user no hay dado commit
-            this.submitted = false;
           },
           error => {
             // se muestra el mensaje alert danger
@@ -229,17 +195,17 @@ export class ConfInicialesComponent implements OnInit {
         )
       }
     }
-
-    this.itemAgregarEditar = new CommonDTO();
-    this.itemAgregarEditar.tipoRegistro = this.tipoRegistro;
-    this.itemAgregarEditar.estado = ESTADO_ACTIVO;
   }
 
+  /**
+   * Metodo que permite eliminar un item seleccionado
+   * @param item , datos del item a eliminar
+   */
   private eliminarItem(item: CommonDTO): void {
 
     // se procede abrir la ventana de confirmacion
     this.confirmationService.confirm({
-      message: '¿Desea eliminar el registro ' + item.nombre + '?',
+      message: '¿Está seguro de que desea eliminar el siguiente registro? <br/> <div class="text_center">' + item.nombre + '</div>',
       header: 'Confirmación',
       icon: 'fa fa-question-circle',
       accept: () => {
@@ -247,19 +213,22 @@ export class ConfInicialesComponent implements OnInit {
         // se muestra el modal de carga
         this.utilService.displayLoading(true);
 
-        // susbripcion para la eliminacion de paciente
+        // susbripcion para la eliminacion del item
         this.adminFreeService.eliminarItem(item).subscribe(
           data => {
-            // se configuran los tipos de viviendas retornados
+            // se configuran los items retornados
             this.items = data.json().result;
 
-            this.alertService.showAlert(data.json().mensaje, "alert alert-success text_center", false);
+            // se reinicia el submmit
+            this.submitted = false;
+
+            // si debe limpiar el item eliminado si fue selecionado por el user
+            if (this.itemAgregarEditar.id && item.id == this.itemAgregarEditar.id) {
+              this.initItemAgregarEditar();
+            }
 
             // se cierra el modal de carga
             this.utilService.displayLoading(false);
-
-            // se inicia como si el user no hay dado commit
-            this.submitted = false;
           },
           error => {
             // se muestra el mensaje alert danger
@@ -273,16 +242,22 @@ export class ConfInicialesComponent implements OnInit {
     });
   }
 
-  private rowSelected(data): void {
-
-
-    this.itemAgregarEditar = new CommonDTO();
-    this.itemAgregarEditar.tipoRegistro = this.tipoRegistro;
-    this.itemAgregarEditar.estado = ESTADO_ACTIVO;
+  /**
+   * Metodo que permite que soportar el evento al momento de seleccionar un item para editar
+   * @param data , es el item a editar
+   */
+  private seleccionarItemEditar(data): void {
+    this.initItemAgregarEditar();
     this.itemAgregarEditar.id = data.id;
     this.itemAgregarEditar.nombre = data.nombre;
+  }
 
-    console.log(this.itemAgregarEditar);
-
+  /**
+   * Metodo que permite inicializar el item de agregacion o edicion
+   */
+  private initItemAgregarEditar(): void {
+    this.itemAgregarEditar = new CommonDTO();
+    this.itemAgregarEditar.tipoRegistro = this.tipoConfiguracion;
+    this.itemAgregarEditar.estado = ESTADO_ACTIVO;
   }
 }
