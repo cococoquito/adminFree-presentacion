1e07a5849bba91e9709efb923ba875e35e7b2cd5
cococoquito <adiz98g@gmail.com>
2017-07-29 12:27:00 -0500

    c

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -2,22 +2,27 @@ import { AppRoot } from './../components/app-root/app-root';
 import { GuardRouting } from './guards/guards-routing';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './../components/app-root/home/home.component';
 import { CambioClaveComponent } from './../components/seguridad/cambio-clave/cambio-clave.component';
+
 import { SolicitarConsecutivoComponent } from './../components/correspondencia/solicitar_consecutivo/solicitar-consecutivo.component';
 import { ArchivoGestionComponent } from './../components/correspondencia/archivo_gestion/archivo-gestion.component';
 import { ConsecutivosSolicitadosComponent } from './../components/correspondencia/consecutivos_solicitados/consecutivos-solicitados.component';
-import { UsuariosComponent } from './../components/admin/usuarios/usuarios.component';
-import { ParametrizacionRegistrosComponent } from './../components/admin/parametrizacion_registros/parametrizacion-registros.component';
-import { FormatoFuenteComponent } from './../components/admin/formato_fuente/formato-fuente.component';
+
+import { TiempoServiciosSalariosComponent } from './../components/solicitud_certificados/tiempo_servicios_salarios/tiempo-servicios-salarios';
 import { CartasLaboralesComponent } from './../components/solicitud_certificados/cartas_laborales/cartas-laborales';
 import { ReporteCesantiasComponent } from './../components/solicitud_certificados/reporte_cesantias/reporte-cesantias';
-import { TiempoServiciosSalariosComponent } from './../components/solicitud_certificados/tiempo_servicios_salarios/tiempo-servicios-salarios';
 import { CertificadosSolicitadosComponent } from './../components/solicitud_certificados/certificados_solicitados/certificados-solicitados';
+
 import { CrearProcesosComponent } from './../components/demandas/crear_procesos/crear-procesos.component';
 import { AgendaAbogadoComponent } from './../components/demandas/agenda_abogado/agenda-abogado.component';
 import { AgendarEventosComponent } from './../components/demandas/agendar_eventos/agendar-eventos.component';
 import { GestionProcesosComponent } from './../components/demandas/gestion_procesos/gestion-procesos.component';
+
+import { UsuariosComponent } from './../components/admin/usuarios/usuarios.component';
+import { ParametrizacionRegistrosComponent } from './../components/admin/parametrizacion_registros/parametrizacion-registros.component';
+import { FormatoFuenteComponent } from './../components/admin/formato_fuente/formato-fuente.component';
 
 const ROUTES: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [GuardRouting] },
