7e70bee1c2b84761094987eb383744a371e1845c
cococoquito <adiz98g@gmail.com>
2017-08-07 14:37:07 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,57 @@
 /**
  * Providers
  */
-import { UtilitarioService } from './service/utilitario.service';
-import { SeguridadService } from './service/seguridad.service';
-import { AlertService } from './service/alert.service';
-import { GuardRouting } from './routing/guards/guards-routing';
-import { AdminFreeService } from './service/admin-free.service';
+import { UtilitarioService } from './b-service/z-common/utilitario.service';
 import { ConfirmationService } from 'primeng/primeng';
+import { AdministradorService } from './b-service/a-admin/administrador.service';
+import { AlertService } from './b-service/z-common/alert.service';
+import { GuardRouting } from './d-routing/guards/guards-routing';
 import { DatePipe } from '@angular/common';
 
 /**
  * imports
  */
-import { AppRoutingModule } from './routing/app-routing.module';
+import { DataTableModule, ConfirmDialogModule, CheckboxModule, DataGridModule } from 'primeng/primeng';
+import { AppRoutingModule } from './d-routing/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
-import { DataTableModule, ConfirmDialogModule, CheckboxModule, DataGridModule } from 'primeng/primeng';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /**
  * declarations
  */
-import { AppRoot } from './components/app-root/app-root';
 import { NgModule } from '@angular/core';
+import { AppRoot } from './a-components/z-app-root/app-root';
 
-import { FooterComponent } from './components/app-root/footer/footer.component';
-import { HomeComponent } from './components/app-root/home/home.component';
-import { AlertComponent } from './components/directivas/alert/alert.component';
-import { LoadingComponent } from './components/directivas/loading/loading.component';
+import { FooterComponent } from './a-components/z-app-root/footer/footer.component';
+import { HomeComponent } from './a-components/z-app-root/home/home.component';
+import { AlertComponent } from './a-components/y-directivas/alert/alert.component';
+import { LoadingComponent } from './a-components/y-directivas/loading/loading.component';
 
-import { LoginComponent } from './components/seguridad/login/login.component';
-import { CambioClaveComponent } from './components/seguridad/cambio-clave/cambio-clave.component';
+import { LoginComponent } from './a-components/a-seguridad/login/login.component';
+import { CambioClaveComponent } from './a-components/a-seguridad/cambio-clave/cambio-clave.component';
 
-import { SolicitarConsecutivoComponent } from './components/correspondencia/solicitar_consecutivo/solicitar-consecutivo.component';
-import { ArchivoGestionComponent } from './components/correspondencia/archivo_gestion/archivo-gestion.component';
-import { ConsecutivosSolicitadosComponent } from './components/correspondencia/consecutivos_solicitados/consecutivos-solicitados.component';
+import { SolicitarConsecutivoComponent } from './a-components/b-correspondencia/solicitar_consecutivo/solicitar-consecutivo.component';
+import { ArchivoGestionComponent } from './a-components/b-correspondencia/archivo_gestion/archivo-gestion.component';
+import { ConsecutivosSolicitadosComponent } from './a-components/b-correspondencia/consecutivos_solicitados/consecutivos-solicitados.component';
 
-import { TiempoServiciosSalariosComponent } from './components/solicitud_certificados/tiempo_servicios_salarios/tiempo-servicios-salarios';
-import { CartasLaboralesComponent } from './components/solicitud_certificados/cartas_laborales/cartas-laborales';
-import { ReporteCesantiasComponent } from './components/solicitud_certificados/reporte_cesantias/reporte-cesantias';
-import { CertificadosSolicitadosComponent } from './components/solicitud_certificados/certificados_solicitados/certificados-solicitados';
+import { TiempoServiciosSalariosComponent } from './a-components/c-solicitud_certificados/tiempo_servicios_salarios/tiempo-servicios-salarios';
+import { CartasLaboralesComponent } from './a-components/c-solicitud_certificados/cartas_laborales/cartas-laborales';
+import { ReporteCesantiasComponent } from './a-components/c-solicitud_certificados/reporte_cesantias/reporte-cesantias';
+import { CertificadosSolicitadosComponent } from './a-components/c-solicitud_certificados/certificados_solicitados/certificados-solicitados';
 
-import { CrearProcesosComponent } from './components/demandas/crear_procesos/crear-procesos.component';
-import { AgendaAbogadoComponent } from './components/demandas/agenda_abogado/agenda-abogado.component';
-import { AgendarEventosComponent } from './components/demandas/agendar_eventos/agendar-eventos.component';
-import { GestionProcesosComponent } from './components/demandas/gestion_procesos/gestion-procesos.component';
+import { CrearProcesosComponent } from './a-components/d-demandas/crear_procesos/crear-procesos.component';
+import { AgendaAbogadoComponent } from './a-components/d-demandas/agenda_abogado/agenda-abogado.component';
+import { AgendarEventosComponent } from './a-components/d-demandas/agendar_eventos/agendar-eventos.component';
+import { GestionProcesosComponent } from './a-components/d-demandas/gestion_procesos/gestion-procesos.component';
 
-import { UsuariosComponent } from './components/admin/usuarios/usuarios.component';
-import { AdminRolesComponent } from './components/admin/usuarios/admin_roles/admin-roles.component';
-import { AdminUsersComponent } from './components/admin/usuarios/admin_usuarios/admin-usuarios.component';
-import { RestablecerClaveComponent } from './components/admin/usuarios/restablecer_clave/restablecer-clave.component';
-import { ParametrizacionRegistrosComponent } from './components/admin/parametrizacion_registros/parametrizacion-registros.component';
-import { FormatoFuenteComponent } from './components/admin/formato_fuente/formato-fuente.component';
+import { UsuariosComponent } from './a-components/a-admin/usuarios/usuarios.component';
+import { AdminRolesComponent } from './a-components/a-admin/usuarios/admin_roles/admin-roles.component';
+import { AdminUsersComponent } from './a-components/a-admin/usuarios/admin_usuarios/admin-usuarios.component';
+import { RestablecerClaveComponent } from './a-components/a-admin/usuarios/restablecer_clave/restablecer-clave.component';
+import { ParametrizacionRegistrosComponent } from './a-components/a-admin/parametrizacion_registros/parametrizacion-registros.component';
+import { FormatoFuenteComponent } from './a-components/a-admin/formato_fuente/formato-fuente.component';
 
 @NgModule({
   declarations: [
@@ -96,11 +95,10 @@ import { FormatoFuenteComponent } from './components/admin/formato_fuente/format
 
   providers: [
     UtilitarioService,
-    SeguridadService,
+    ConfirmationService,
+    AdministradorService,
     AlertService,
     GuardRouting,
-    AdminFreeService,
-    ConfirmationService,
     DatePipe
   ],
   bootstrap: [AppRoot]
