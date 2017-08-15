954ee079a7a9daf1dd344c29ffdedf648c320fc7
cococoquito <adiz98g@gmail.com>
2017-08-14 22:22:29 -0500

    commit

--- src/app/a-components/a-admin/usuarios/admin_usuarios/admin-usuarios.component.ts
+++ src/app/a-components/a-admin/usuarios/admin_usuarios/admin-usuarios.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UsuariosDTO } from './../../../../c-model/a-admin/usuarios/UsuariosDTO';
+import { ConfirmationService } from 'primeng/primeng';
+import { AlertService } from './../../../../b-service/z-common/alert.service';
+import { AdministradorService } from './../../../../b-service/a-admin/administrador.service';
+import { UtilitarioService } from './../../../../b-service/z-common/utilitario.service';
+import { Component, OnInit } from '@angular/core';
 
 /**
  * Componente para la administracion de los Usuarios del sistema
@@ -7,16 +12,53 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
     selector: 'app-admin-users',
     templateUrl: './admin-usuarios.component.html'
 })
-export class AdminUsersComponent implements OnInit, OnDestroy {
+export class AdminUsersComponent implements OnInit {
+
+    /**lista de usuarios parametrizados en el sistema*/
+    private usuarios: Array<UsuariosDTO>;
+
+    /**
+    * Constructor del componente para la administracion del ROL
+    * @param utilService, service con las funciones utilitarias
+    * @param administradorService, contiene los servicios administrativo
+    * @param alertService, service para la comunicacion del componente de mensaje de alerta
+    * @param confirmationService, servicio para la visualizacion del modal de confirmacion
+    */
+    constructor(
+        private utilService: UtilitarioService,
+        private administradorService: AdministradorService,
+        private alertService: AlertService,
+        private confirmationService: ConfirmationService) { }
 
     /**
      * PostConstructor que permite inicializar las variables del component
      */
     ngOnInit(): void {
-        console.log("se init el admin-users");
+
+        // se lista los usuarios del sistema
+        this.getUsuarios();
     }
 
-    ngOnDestroy(): void {
-        console.log("se destruyo el admin-users");
+    /**
+     * Metodo que permite obtener todos los USUARIOS ACTIVOS parametrizados en el sistema
+     */
+    private getUsuarios(): void {
+
+        // se muestra el modal de carga
+        this.utilService.displayLoading(true);
+
+        // se invoca el servicio para obtener los USUARIOS ACTIVOS
+        this.administradorService.listarUsuariosActivos().subscribe(
+            data => {
+                // se construye los usuarios consultados
+                this.usuarios = data.json();
+
+                // se cierra el modal de carga
+                this.utilService.displayLoading(false);
+            },
+            error => {
+                this.utilService.showErrorSistema(error, this.alertService);
+            }
+        );
     }
 }
---
+++ src/app/c-model/a-admin/usuarios/UsuariosDTO.ts
@@ -0,0 +1,26 @@
+
+/**
+ * DTO que contiene las propiedades del usuario ACTIVO
+ * 
+ * @author Carlos Andres Diaz
+ */
+export class UsuariosDTO {
+
+    /** representa el identificador del usuario **/
+    public idUsuario: number;
+
+    /** representa el nombre del usuario **/
+    public nombre: string;
+
+	/** representa el nombre Usuario del para seguridad del user **/
+	private nombreUsuario: string;    
+
+    /** representa el identificador del ROL del usuario **/
+    public roles: number;
+
+    /** 1= si el usuario es abogado **/
+    public esAbogado: number;
+
+    /** representa el nombre del ROL del usuario **/
+    public nombreRole: string;
+}
